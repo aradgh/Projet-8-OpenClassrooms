5d850a03a667ba2c3725e35e5cbec6cccf3d75d4
aradgh <arad.ghorreshi@gmail.com>
2025-08-01 03:38:08 +0200

    Ajoute l'annotation `@Test` à `getTripDeals` dans `TestTourGuideService` et corrige l'assertion pour vérifier 5 fournisseurs au lieu de 10.

--- src/test/java/com/openclassrooms/tourguide/TestTourGuideService.java
+++ src/test/java/com/openclassrooms/tourguide/TestTourGuideService.java
@@ -107,7 +107,8 @@ class TestTourGuideService {
 		assertEquals(5, attractions.size());
 	}
 
-	public void getTripDeals() {
+	@Test
+	void getTripDeals() {
 		GpsUtil gpsUtil = new GpsUtil();
 		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
 		InternalTestHelper.setInternalUserNumber(0);
@@ -119,7 +120,7 @@ class TestTourGuideService {
 
 		tourGuideService.tracker.stopTracking();
 
-		assertEquals(10, providers.size());
+		assertEquals(5, providers.size());
 	}
 
 }
